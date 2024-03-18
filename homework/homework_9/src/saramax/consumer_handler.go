86d819e60beea69819e3e12243159119fee0158f
Jared.Lu <jared-lu@outlook.com>
2024-03-18 09:54:51 +0800

    第九次作业

---
+++ homework/homework_9/src/saramax/consumer_handler.go
@@ -0,0 +1,81 @@
+package saramax
+
+import (
+	"encoding/json"
+	"github.com/IBM/sarama"
+	"github.com/prometheus/client_golang/prometheus"
+	"time"
+	"webook/webook/pkg/logger"
+)
+
+type HandlerV1[T any] func(msg *sarama.ConsumerMessage, t T) error
+
+type Handler[T any] struct {
+	l      logger.Logger
+	fn     func(msg *sarama.ConsumerMessage, t T) error
+	vector *prometheus.SummaryVec
+}
+
+func NewHandler[T any](l logger.Logger, fn func(msg *sarama.ConsumerMessage, t T) error) *Handler[T] {
+	vector := prometheus.NewSummaryVec(prometheus.SummaryOpts{
+		Namespace: "geekbang_daming",
+		Subsystem: "webook",
+		Name:      "kafka_consume_time",
+		Help:      "统计 Kafka 消费者的消费时间",
+	}, []string{"biz"})
+	return &Handler[T]{
+		l:      l,
+		fn:     fn,
+		vector: vector,
+	}
+}
+
+func (h Handler[T]) Setup(session sarama.ConsumerGroupSession) error {
+	return nil
+}
+
+func (h Handler[T]) Cleanup(session sarama.ConsumerGroupSession) error {
+	return nil
+}
+
+func (h Handler[T]) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+	msgs := claim.Messages()
+	for msg := range msgs {
+		startTime := time.Now()
+		var t T
+		err := json.Unmarshal(msg.Value, &t)
+		if err != nil {
+			h.l.Error("反序列化消息失败",
+				logger.Error(err),
+				logger.String("topic", msg.Topic),
+				logger.Int64("partition", int64(msg.Partition)),
+				logger.Int64("offset", msg.Offset))
+			continue
+		}
+		// 在这里执行重试
+		for i := 0; i < 3; i++ {
+			err = h.fn(msg, t)
+			duration := time.Since(startTime).Milliseconds()
+			h.vector.WithLabelValues("consumer_time").Observe(float64(duration))
+			if err == nil {
+				break
+			}
+			h.l.Error("处理消息失败",
+				logger.Error(err),
+				logger.String("topic", msg.Topic),
+				logger.Int64("partition", int64(msg.Partition)),
+				logger.Int64("offset", msg.Offset))
+		}
+
+		if err != nil {
+			h.l.Error("处理消息失败-重试次数上限",
+				logger.Error(err),
+				logger.String("topic", msg.Topic),
+				logger.Int64("partition", int64(msg.Partition)),
+				logger.Int64("offset", msg.Offset))
+		} else {
+			session.MarkMessage(msg, "")
+		}
+	}
+	return nil
+}
