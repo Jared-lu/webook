cd4d5cf98539863faec8c3ba7ef275e0ac0f5af5
Jared.lu <jared-lu@outlook.com>
2024-03-24 21:22:32 +0800

    第十九次作业

---
+++ webook/internal/events/mysql_binlog_event.go
@@ -0,0 +1,59 @@
+package events
+
+import (
+	"context"
+	"github.com/IBM/sarama"
+	"time"
+	"webook/webook/internal/repository/cache"
+	"webook/webook/pkg/logger"
+)
+
+type MySQLBinlogConsumer struct {
+	client sarama.Client
+	l      logger.Logger
+	cache  cache.FollowCache
+}
+
+func (r *MySQLBinlogConsumer) Start() error {
+	cg, err := sarama.NewConsumerGroupFromClient("follow_relations_cache",
+		r.client)
+	if err != nil {
+		return err
+	}
+	go func() {
+		err := cg.Consume(context.Background(),
+			[]string{"webook_binlog"},
+			saramax.NewHandler[canalx.Message[dao.FollowRelation]](r.l, r.Consume))
+		if err != nil {
+			r.l.Error("退出了消费循环异常", logger.Error(err))
+		}
+	}()
+	return err
+}
+
+func (r *MySQLBinlogConsumer) Consume(msg *sarama.ConsumerMessage,
+	val canalx.Message[dao.FollowRelation]) error {
+	// 只监听 follow_relations 表
+	if val.Table != "follow_relations" {
+		return nil
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	for _, data := range val.Data {
+		var err error
+		switch data.Status {
+		case dao.FollowRelationStatusActive:
+			// 在缓存中创建关注关系
+			err = r.cache.Follow(ctx, data.Follower, data.Followee)
+		case dao.FollowRelationStatusInactive:
+			// 在缓存中取消关注关系
+			err = r.cache.CancelFollow(ctx, data.Follower, data.Followee)
+		default:
+			err = errors.New("unknown status")
+		}
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
--- webook/internal/repository/cache/types.go
+++ webook/internal/repository/cache/types.go
@@ -41,3 +41,10 @@ type ArticleCache interface {
 //	Get(ctx context.Context, key string) (any, error)
 //	Set(ctx context.Context, key string, val any, expiration time.Duration)error
 //}
+
+type FollowCache interface {
+	StaticsInfo(ctx context.Context, uid int64) (domain.FollowStatics, error)
+	SetStaticsInfo(ctx context.Context, uid int64, statics domain.FollowStatics) error
+	Follow(ctx context.Context, follower, followee int64) error
+	CancelFollow(ctx context.Context, follower, followee int64) error
+}
