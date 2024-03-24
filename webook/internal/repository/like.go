bb5da5e67e6166613f7dd7a44a7eb1c4f77ccd89
Jared.lu <jared-lu@outlook.com>
2024-03-24 20:47:51 +0800

    第十八次作业

--- webook/internal/repository/like.go
+++ webook/internal/repository/like.go
@@ -1,1 +1,25 @@
 package repository
+
+import (
+	"context"
+	"webook/webook/internal/domain"
+	"webook/webook/internal/repository/dao"
+)
+
+type likeRepository struct {
+	dao dao.LikeDAO
+}
+
+func (l *likeRepository) InputLike(ctx context.Context, msg domain.Like) error {
+	return l.dao.InputLike(ctx, dao.Like{
+		Uid:    msg.Uid,
+		Biz:    msg.Biz,
+		BizId:  msg.BizId,
+		Status: msg.Status,
+	})
+}
+
+func NewLikeRepository(d dao.LikeDAO) LikeRepository {
+
+	return &likeRepository{dao: d}
+}
